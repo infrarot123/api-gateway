f3e44aad6533c631dcead48bb3243058c441f34f
Moritz Schauer <moritz.schauer@chrono24.com>
2021-03-26 13:50:31 +0100

    initial commit

---
+++ src/test/java/com/mos/cloud/gateway/CloudGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mos.cloud.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
